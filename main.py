0bfa8bee378cfaafb33cdd1e745d0200c6d23e74
Martin <mfra287@wgu.edu>
2024-07-29 13:13:57 -0400

    Created a RESTful API using FastAPI

--- main.py
+++ main.py
@@ -26,21 +26,23 @@ class Data(BaseModel):
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
     native_country: str = Field(..., example="United-States", alias="native-country")
 
-path = # TODO: enter the path for the saved encoder 
+# TODO: enter the path for the saved encoder 
+path = os.path.join("model", "encoder.pkl")
 encoder = load_model(path)
 
-path = # TODO: enter the path for the saved model 
+# TODO: enter the path for the saved model 
+path = os.path.join("model", "model.pkl")
 model = load_model(path)
 
 # TODO: create a RESTful API using FastAPI
-app = # your code here
+app = FastAPI()
 
 # TODO: create a GET on the root giving a welcome message
 @app.get("/")
 async def get_root():
     """ Say hello!"""
     # your code here
-    pass
+    return {"Welcome to Fast API"}
 
 
 # TODO: create a POST on a different path that does model inference
@@ -69,6 +71,11 @@ async def post_inference(data: Data):
         # use data as data input
         # use training = False
         # do not need to pass lb as input
+        data,
+        categorical_features=cat_features,
+        training=False,
+        encoder=encoder
     )
-    _inference = # your code here to predict the result using data_processed
+    # your code here to predict the result using data_processed
+    _inference = model.predict(data_processed)
     return {"result": apply_label(_inference)}
